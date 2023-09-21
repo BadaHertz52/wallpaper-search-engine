d03e70d9c0a804e2e99be37583802caa1ef3e8f3
badahertz52 <badahertz52@gmail.com>
2023-09-21 23:08:21 +0900

    [fn] To correct the error in which dumyData could not be loaded, Update to get new image data from pixabay when refreshing or when the site is first opened.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import Hero from './component/Hero';
 import ResultContainer from './component/ResultContainer';
 import Footer from './component/Footer';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ModalState, ResponseData } from './type';
 import DummyData from './asset/dummyData';
+import { storageKey } from './storageKey';
+import { getImgData } from './fn';
 
 const Container = styled.div`
     position: relative;
@@ -15,12 +17,27 @@ const Container = styled.div`
 `;
 
 function App() {
-    const [data, setData] = useState<ResponseData | null>(DummyData);
+    const [data, setData] = useState<ResponseData | null>(null);
 
     const [modal, setModal] = useState<ModalState>({
         open: false,
         targetImgData: undefined,
     });
+    const recentKeywords = localStorage.getItem(storageKey.searchWords);
+
+    useEffect(() => {
+        const keywords = recentKeywords
+            ? (JSON.parse(recentKeywords) as string[])[0]
+            : 'dog';
+        (async () => {
+            const imgData = await getImgData(keywords);
+            if (imgData instanceof Error || !imgData.totalHits) {
+                setData(null);
+            } else {
+                setData(imgData);
+            }
+        })();
+    }, [recentKeywords]);
     return (
         <>
             <Container>
