f133ae4c4702398455232de4d3323d207c356814
badahertz52 <badahertz52@gmail.com>
2023-09-24 18:52:38 +0900

    [style]  Modify size of img in ImageModal

--- src/component/ImageModal.tsx
+++ src/component/ImageModal.tsx
@@ -13,17 +13,18 @@ const Modal = styled.div`
     border-radius: 8px;
     color: var(--text);
     min-width: 300px;
-    max-width: 100%;
-    max-height: 100%;
+    max-width: 60vw;
+    max-height: 90vh;
     overflow: auto;
     background-color: var(--primary);
     border: 3px solid var(--secondary);
     padding: 16px;
     box-shadow: 8px 8px 12px -1px rgb(0 0 0 / 0.3);
+    overflow: hidden;
 `;
-
 const ModalImg = styled.img`
     width: 100%;
+    max-width: 100%;
 `;
 
 const DetailRow = styled.div`
@@ -32,7 +33,6 @@ const DetailRow = styled.div`
         margin-right: 6px;
     }
 `;
-
 type ImageModalProps = {
     modal: ModalState;
     setModal: Dispatch<SetStateAction<ModalState>>;
@@ -44,12 +44,13 @@ const ImageModal = ({ modal, setModal }: ImageModalProps) => {
         likes: '',
         views: '',
     };
+    // window.innerHeight - modal padding * 2 - deleteIcon height - other height
+    const maxHeight = window.innerHeight - 16 * 2 - 24 - 21.6 * 3 - 16 * 4 - 16;
     const largeImageURL: string | undefined = imgData?.largeImageURL;
     const modalRootEl = document.getElementById('modal-root') as HTMLElement;
     const closeModal = useCallback(() => {
         setModal({ open: false, targetImgData: undefined });
     }, [setModal]);
-
     return reactDOM.createPortal(
         <Modal style={{ display: modal.open ? 'block' : 'none' }}>
             <DeleteIcon
@@ -58,13 +59,13 @@ const ImageModal = ({ modal, setModal }: ImageModalProps) => {
                 fill="#FFFFFF"
                 onClick={closeModal}
             />
-            <ModalImg src={largeImageURL} />
-            <p>{tags}</p>
+            <ModalImg src={largeImageURL} style={{ maxHeight: maxHeight }} />
+            <p style={{ margin: '10px 0' }}>{tags}</p>
             <DetailRow>
                 <LikeIcon width="20px" height="20px" />
                 {likes}명이 좋아합니다
             </DetailRow>
-            <p>{views} 조회</p>
+            <p style={{ margin: '10px 0' }}>{views} 조회</p>
         </Modal>,
         modalRootEl
     );
