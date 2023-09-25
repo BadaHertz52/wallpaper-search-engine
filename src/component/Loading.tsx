413af8fe0b12017d24bf4e8dae9b014fde81764a
badahertz52 <badahertz52@gmail.com>
2023-09-25 23:32:19 +0900

    [style] Change color  of Loading  by dark mode

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import React from 'react';
 import styled, { CSSProperties, keyframes } from 'styled-components';
 
 const loading = keyframes`
@@ -12,10 +12,10 @@ const Loader = styled.div`
     width: 5rem;
     height: 5rem;
     border-radius: 50%;
-    background-color: #ffff;
+    background-color: var(--primary);
     animation: ${loading} 2s linear infinite;
 `;
-const CircleLeft = styled.div`
+const Circle = styled.div`
     &::before {
         content: '';
         position: absolute;
@@ -23,7 +23,7 @@ const CircleLeft = styled.div`
         right: -5px;
         width: calc((5rem - 60px) / 2);
         height: calc((5rem - 60px) / 2);
-        background-color: rgba(50, 50, 50, 0.966);
+        background-color: var(--text);
         border-radius: 50%;
     }
     position: absolute;
@@ -33,22 +33,9 @@ const CircleLeft = styled.div`
     height: 100%;
     border-top-left-radius: 100px;
     border-bottom-left-radius: 100px;
-    background: linear-gradient(
-        to top,
-        rgba(223, 223, 223),
-        rgba(50, 50, 50, 0.966)
-    );
+    background: linear-gradient(to top, transparent, var(--text));
 `;
-const CircleRight = styled.div`
-    position: absolute;
-    top: 0;
-    left: 50%;
-    width: 50%;
-    height: 100%;
-    border-top-right-radius: 100px;
-    border-bottom-right-radius: 100px;
-    background: linear-gradient(to top, rgb(216, 215, 215), transparent);
-`;
+
 const LoaderSpan = styled.span`
     position: absolute;
     top: 10px;
@@ -69,13 +56,13 @@ function Loading() {
         width: '100%',
         height: '100$',
     };
+
     return (
         <div className="loading" style={loadingStyle}>
             <h3 style={{ textAlign: 'center' }}>Loading...</h3>
             <Loader>
-                <CircleLeft />
+                <Circle />
                 <LoaderSpan></LoaderSpan>
-                <CircleRight />
             </Loader>
         </div>
     );
